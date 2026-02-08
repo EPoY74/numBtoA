f0dfd389661f389c5f5174923d7e4204c228f898
Evgenii Petrov IceBook <epoy74@gmail.com>
2026-02-08 17:59:25 +0400

    feat(tests): add cases with multiple zeros in B

--- src/main.py
+++ src/main.py
@@ -5,9 +5,39 @@
 """
 
 
-def main():
-    pass
+def printInfo(info: str):
+    print(info, end=" ")
+
+
+def inputNums() -> tuple[str, str]:
+    while True:
+        try:
+            printInfo("Введите число А:")
+            numA: str = input().strip()
+        except Exception as err:
+            raise err
+        break
+
+    while True:
+        try:
+            printInfo("Введите число В:")
+            numB: str = input().strip()
+            break
+        except Exception as err:
+            raise err
+        break
+    return (numA, numB)
+
+
+def main(numA: str, numB: str) -> str:
+    print(numA)
+    print(numB)
+    return "1"
 
 
 if __name__ == "__main__":
-    pass
+    inputNumA: str = ""
+    inputNumB: str = ""
+    inputNumA, inputNumB = inputNums()
+
+    main(inputNumA, inputNumB)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,33 @@
 from src.main import main
 
 
-def test_input() -> None:
+def test_2positive() -> None:
     numA = "90817"
     numB = "765"
     result = main(numA, numB)
-    expected = "1"
+    expected = "97867"
+    assert result == expected
 
+
+def test_negative_and_positive() -> None:
+    numA = "-8350"
+    numB = "0129"
+    result = main(numA, numB)
+    expected = "-0120"
+    assert result == expected
+
+
+def test_2positive_many_zeros() -> None:
+    numA = "90817"
+    numB = "0009"
+    result = main(numA, numB)
+    expected = "99817"
+    assert result == expected
+
+
+def test_negative_and_positive_many_zeros() -> None:
+    numA = "-8350"
+    numB = "0009"
+    result = main(numA, numB)
+    expected = "0000"
     assert result == expected
