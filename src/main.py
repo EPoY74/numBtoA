4e2ba878bf9689d9c7114e9bc502bba10f312382
Evgenii Petrov IceBook <epoy74@gmail.com>
2026-02-09 21:58:41 +0400

    feat(main): All tests passed

--- src/main.py
+++ src/main.py
@@ -5,39 +5,54 @@
 """
 
 
-def printInfo(info: str):
-    print(info, end=" ")
-
-
-def inputNums() -> tuple[str, str]:
-    while True:
-        try:
-            printInfo("Введите число А:")
-            numA: str = input().strip()
-        except Exception as err:
-            raise err
-        break
-
-    while True:
-        try:
-            printInfo("Введите число В:")
-            numB: str = input().strip()
-            break
-        except Exception as err:
-            raise err
-        break
-    return (numA, numB)
-
-
 def main(numA: str, numB: str) -> str:
-    print(numA)
-    print(numB)
-    return "1"
+    result: list[str] = []
+    is_all_zeros: bool = False
+    numB_sorted_asc = sorted(numB)
+    numB_sorted_dec = sorted(numB, reverse=True)
+    # numA_list =
+    my_str: str = ""
+    is_negative: bool = False
+
+    for index, oneDig in enumerate(numA):
+        if (oneDig == "-") and (index == 0):
+            is_negative = True
+            continue
+        if is_negative:
+            if not numB_sorted_dec:
+                result.append(oneDig)
+                continue
+
+            if oneDig > numB_sorted_dec[-1]:
+                result.append(numB_sorted_dec[-1])
+                numB_sorted_dec.pop(-1)
+            else:
+                result.append(oneDig)
+
+        else:
+            if not numB_sorted_asc:
+                result.append(oneDig)
+                continue
+
+            if oneDig < numB_sorted_asc[-1]:
+                result.append(numB_sorted_asc[-1])
+                # result = numB_sorted[-1]
+                numB_sorted_asc.pop(-1)
+            else:
+                result.append(oneDig)
+
+    if all(ch == "0" for ch in result):
+        is_all_zeros = True
+    my_str = "".join(result)
+    if is_negative and (not is_all_zeros):
+        my_str = "-" + my_str
+    return str(my_str)
 
 
 if __name__ == "__main__":
-    inputNumA: str = ""
-    inputNumB: str = ""
-    inputNumA, inputNumB = inputNums()
+    inputNumA: str = "8350"
+    inputNumB: str = "0129"
+    # inputNumA, inputNumB = inputNums()
 
-    main(inputNumA, inputNumB)
+    result = main(inputNumA, inputNumB)
+    print(result)
